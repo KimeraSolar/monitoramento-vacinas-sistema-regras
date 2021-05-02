fa386cd428f10f5f38518bf02c7f9b45a72b6951
Alan Felix <felixnascc@gmail.com>
2021-05-02 15:56:54 -0300

    Retirada do conteúdo gigante do styles.css e ajustes para eliminar arquivos desnecessários

--- client-front-end/js/scripts.js
+++ client-front-end/js/scripts.js
@@ -1,3 +1,19 @@
+// Initialize and add the map
+function initMap() {
+    // The location of Uluru
+    const uluru = { lat: -25.344, lng: 131.036 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: uluru,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+    });
+}
+
 function createChamber(chb) {
     let container = document.createElement("div")
     container.className = "col-xl-3"
