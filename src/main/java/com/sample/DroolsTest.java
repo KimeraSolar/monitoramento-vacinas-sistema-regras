5d29f609c6cf9fac3af94bbd879ed7b6f71f3b2e
LBBassani <lorenabassani12@gmail.com>
2021-05-03 20:33:17 -0300

    Arquivos de entrada xml

--- src/main/java/com/sample/DroolsTest.java
+++ src/main/java/com/sample/DroolsTest.java
@@ -1,23 +1,27 @@
 package com.sample;
 
-import java.io.BufferedReader;  
-import java.io.FileReader;
+import java.io.File;
 import java.io.IOException;
 import java.net.URI;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.Map;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.grizzly.http.server.StaticHttpHandler;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.ext.Provider;
+import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
@@ -28,6 +32,7 @@ public class DroolsTest {
 	
 	// Base URI the Grizzly HTTP server will listen on
     public static final String BASE_URI = "http://localhost:8080/vacinas/";
+    public static final String INPUT_FILE = "config\\teste_01.xml";
     
     @Provider
     public static class CORSFilter implements ContainerResponseFilter {
@@ -77,75 +82,93 @@ public class DroolsTest {
     	    KieContainer kContainer = ks.getKieClasspathContainer();
         	KieSession kSession = kContainer.newKieSession("ksession-rules");
         	
-        	List<Vacina.TipoVacina> tipos = new LinkedList<Vacina.TipoVacina>();
-        	List<Camara> camaras = new LinkedList<Camara>();
-        	List<Gerente> gerentes = new LinkedList<Gerente>();
-        	List<Vacina> vacinas = new LinkedList<Vacina>();
+        	Map<String, Vacina.TipoVacina> tipos = new HashMap<String,Vacina.TipoVacina>();
+        	Map<String, String> gpsMode = new HashMap<String, String>();
+        	Map<String, String> tempMode = new HashMap<String, String>();
         	
         	Resources.gerentes = new HashMap<String, Gerente>();
         	Resources.camaras = new HashMap<String, Camara>();
         	
-        	// TODO: ler a entrada de arquivo xml
         	// Leitura do arquivo de configuração de vacinas
-        	Vacina.TipoVacina tipo1 = new Vacina.TipoVacina("CovidShield", -2, 8, 5000), tipo2 = new Vacina.TipoVacina("CoronaVac", -4, 6, 10000); 
+        	File file = new File(INPUT_FILE);
+        	Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(file);
         	
-        	tipos.add(tipo1);
-        	tipos.add(tipo2);
-        	
-        	Gerente g1 = new Gerente("g01", "Gerente 1"), g2 = new Gerente("g02", "Gerente 2");
-        	gerentes.add(g1); gerentes.add(g2);
-        	
-        	for( Gerente g : gerentes) {
-        		Resources.gerentes.put(g.getObjectId(), g); 
-        		kSession.insert(g);
+        	// Leitura dos tipos de vacina
+        	for( int i = 0; i < document.getElementsByTagName("tipo-vacina").getLength(); i++) {
+        		NamedNodeMap n = document.getElementsByTagName("tipo-vacina").item(i).getAttributes();
+        		
+        		String nome = n.getNamedItem("nome").getTextContent();
+        		float tempMin = Float.parseFloat(n.getNamedItem("tempMin").getTextContent());
+        		float tempMax = Float.parseFloat(n.getNamedItem("tempMax").getTextContent());
+        		long tempo = Long.parseLong(n.getNamedItem("tempo").getTextContent());
+        		
+        		tipos.put(nome, new Vacina.TipoVacina(nome, tempMin, tempMax, tempo));
         	}
         	
-        	Camara c1 = new Camara("c01"), c2 = new Camara("c02"), c3 = new Camara("c03");
-        	
-        	c1.addGerente(g1); g1.addCamara(c1);
-        	c1.addGerente(g2); g2.addCamara(c1);
-        	c2.addGerente(g1); g1.addCamara(c2);
-        	c3.addGerente(g1); g1.addCamara(c3);
-        	c3.addGerente(g2); g2.addCamara(c3);
-        	
-        	camaras.add(c1); camaras.add(c2); camaras.add(c3);
-        	
-        	for(Camara c : camaras) {
-        		Resources.camaras.put(c.getObjectId(), c);
-        		kSession.insert(c);
+        	for( int i = 0; i < document.getElementsByTagName("gerente").getLength(); i++) {
+        		NamedNodeMap n = document.getElementsByTagName("gerente").item(i).getAttributes();
+        		
+        		String nome = n.getNamedItem("nome").getTextContent();
+        		String id = n.getNamedItem("id").getTextContent();
+        		String mode = n.getNamedItem("gps-sensor-mode").getTextContent();
+        		
+        		Gerente g = new Gerente(id, nome);
+        		Resources.gerentes.put(id, g);
+        		gpsMode.put(id, mode);
         	}
         	
-        	Vacina v1 = new Vacina(tipo1, new Date(), null, false), 
-				   v2 = new Vacina(tipo1, new Date(), null, false), 
-        		   v3 = new Vacina(tipo2, new Date(), null, false), 
-        		   v4 = new Vacina(tipo2, new Date(), null, false), 
-        		   v5 = new Vacina(tipo2, new Date(), null, false);
+        	for( int i = 0; i < document.getElementsByTagName("camara").getLength(); i++) {
+        		NamedNodeMap n = document.getElementsByTagName("camara").item(i).getAttributes();
+        		
+        		String id = n.getNamedItem("id").getTextContent();
+        		String gps = n.getNamedItem("gps-sensor-mode").getTextContent();
+        		String temp = n.getNamedItem("temp-sensor-mode").getTextContent();
+        		
+        		Camara c = new Camara(id);
+        		Resources.camaras.put(id, c);
+        		gpsMode.put(id, gps);
+        		tempMode.put(id, temp);
+        	}
         	
-        	c1.addVacina(v1); c1.addVacina(v2); c1.addVacina(v3);
-        	c2.addVacina(v4); c2.addVacina(v5);
-        	
-        	vacinas.add(v1); vacinas.add(v2); vacinas.add(v3);
-        	vacinas.add(v4); vacinas.add(v5);
-        	
-        	for(Vacina v : vacinas) {
+        	for( int i = 0; i < document.getElementsByTagName("vacina").getLength(); i++) {
+        		NamedNodeMap n = document.getElementsByTagName("vacina").item(i).getAttributes();
+        		
+        		String tipo = n.getNamedItem("tipo").getTextContent();
+        		String c = n.getNamedItem("camara").getTextContent();
+        		
+        		Vacina v = new Vacina(tipos.get(tipo), new Date(), null, false);
         		kSession.insert(v);
+        		Resources.camaras.get(c).addVacina(v);
         	}
         	
+        	for( int i = 0; i < document.getElementsByTagName("responsabilidade").getLength(); i++) {
+        		NamedNodeMap n = document.getElementsByTagName("responsabilidade").item(i).getAttributes();
+        		
+        		String gerente = n.getNamedItem("gerente").getTextContent();
+        		String camara = n.getNamedItem("camara").getTextContent();
+        		
+        		Camara c = Resources.camaras.get(camara);
+        		Gerente g = Resources.gerentes.get(gerente);
+        		c.addGerente(g); g.addCamara(c);
+        	}
+
         	// Inicialização dos sensores
         	List<Thread> threads = new LinkedList<Thread>();
 
         	int sensorid = 0;
-            for (Camara c : camaras) {
+            for (Map.Entry<String, Camara> entry : Resources.camaras.entrySet()) {
+            	Camara c = entry.getValue();
             	FactHandle f = kSession.insert(c);
-            	threads.add(new Thread(new GpsSensorWrapper("s" + String.format("%01d", sensorid), kSession, f, "static")));
+            	threads.add(new Thread(new GpsSensorWrapper("s" + String.format("%01d", sensorid), kSession, f, gpsMode.get(entry.getKey()))));
             	sensorid += 1;
-            	threads.add(new Thread(new TempSensorWrapper("s" + String.format("%01d", sensorid), kSession, f, "random")));
+            	threads.add(new Thread(new TempSensorWrapper("s" + String.format("%01d", sensorid), kSession, f, tempMode.get(entry.getKey()))));
             	sensorid += 1;
             }
             
-            for (Gerente g : gerentes) {
+            for (Map.Entry<String, Gerente> entry : Resources.gerentes.entrySet()) {
+            	Gerente g = entry.getValue();
             	FactHandle f = kSession.insert(g);
-            	threads.add(new Thread(new GpsSensorWrapper("s" + String.format("%01d", sensorid), kSession, f, "random")));
+            	threads.add(new Thread(new GpsSensorWrapper("s" + String.format("%01d", sensorid), kSession, f, gpsMode.get(entry.getKey()))));
             	sensorid += 1;
             }
             
@@ -163,7 +186,6 @@ public class DroolsTest {
             	t.join();
             }
             server.shutdown();
-            
         } catch (Throwable t) {
             t.printStackTrace();
         }
